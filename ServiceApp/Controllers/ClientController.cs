d1f8445fd9893af278aeb698f359780f36400352
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-09-29 13:16:43 +0200

    add route by annotation

--- ServiceApp/Controllers/ClientController.cs
+++ ServiceApp/Controllers/ClientController.cs
@@ -4,6 +4,8 @@ using System.Text.Json;
 
 namespace ServiceApp.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ClientController: ControllerBase
     {
         private readonly ServiceAppContext _context;
@@ -11,7 +13,8 @@ namespace ServiceApp.Controllers
         {
             _context = context;
         }
-
+        [HttpGet]
+        [Route("clients")]
         public async Task<IActionResult> GetAllClients()
         {
             if (_context.Clients.Any())
@@ -21,7 +24,7 @@ namespace ServiceApp.Controllers
             }
             else
             {
-                return NotFound();
+                return NotFound("No found any clients");
             }
         }
     }
