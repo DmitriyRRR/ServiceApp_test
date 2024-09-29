9382bfdf7ffe4143969896afe58d58dc986df0ef
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-09-29 09:41:59 +0200

    forgot save the models..

--- ServiceApp/Models/Clients.cs
+++ ServiceApp/Models/Clients.cs
@@ -2,5 +2,7 @@
 {
     public class Clients
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- ServiceApp/Models/Devices.cs
+++ ServiceApp/Models/Devices.cs
@@ -1,6 +1,14 @@
-﻿namespace ServiceApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServiceApp.Models
 {
     public class Devices
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("ClientId")]
+        public int ClientId { get; set; } = 0; //temporary, db schemes will be maintance and change
     }
 }
