e8f911dcc37e5b66d3327c35fa31a476edb613fb
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-09-29 12:28:17 +0200

    minor updates

---
+++ ServiceApp/Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceApp.Models;
+using System.Text.Json;
+
+namespace ServiceApp.Controllers
+{
+    public class ClientController: ControllerBase
+    {
+        private readonly ServiceAppContext _context;
+        public ClientController(ServiceAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> GetAllClients()
+        {
+            if (_context.Clients.Any())
+            {
+                var clients = _context.Clients.ToList();
+                return (IActionResult)clients;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- ServiceApp/ServiceAppContext.cs
+++ ServiceApp/ServiceAppContext.cs
@@ -5,13 +5,28 @@ namespace ServiceApp
 {
     public class ServiceAppContext : DbContext
     {
-        public DbSet<Client> Clients { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Device> Devices { get; set; }
-        public DbSet<Part> Parts { get; set; }
+        public DbSet<Client> Clients { get; set; } = null;
+        public DbSet<User> Users { get; set; } = null;
+        public DbSet<Device> Devices { get; set; } = null;
+        public DbSet<Part> Parts { get; set; } = null;
 
-        public ServiceAppContext(DbContextOptions<ServiceAppContext> options):base(options)
+        public ServiceAppContext(DbContextOptions<ServiceAppContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().HasData(
+                new Client { Id = 1, Name = "Jhon" },
+                new Client { Id = 2, Name = "Den" },
+                new Client { Id = 3, Name = "Tom" }
+                );
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Sam", Email="sam@mail.com" },
+                new User { Id = 2, Name = "Scott", Email="scott@gmail.com"},
+                new User { Id = 3, Name = "Bill", Email="bill@yahoo.com" }
+                );
         }
     }
 }
