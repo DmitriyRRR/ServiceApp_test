ab843acd8043e12746d928c8d940a789d8758ee8
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-09-29 09:38:29 +0200

    filled in the models

--- ServiceApp/Models/Parts.cs
+++ ServiceApp/Models/Parts.cs
@@ -1,6 +1,13 @@
-﻿namespace ServiceApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServiceApp.Models
 {
     public class Parts
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // one-to many devices-parts?
     }
 }
