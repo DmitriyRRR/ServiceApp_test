64f8f4bbeb66aba52bc6cb5aedba427cac69ef3e
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2024-10-03 17:50:40 +0200

    AddImplemetRepo

---
+++ ServiceApp/Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ServiceApp.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
