964989b84581be741c682f8da41794390835b92d
Dmitriy Romanov <romanov.dmitriy.ua@gmail.com>
2025-02-05 20:54:07 +0100

    something after a long pause

---
+++ ServiceApp.Test/ClientRepositoryTest.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceApp.Test
+
+{
+    public class ClientRepositoryTest
+    {
+        public void Test1()
+        {
+
+        }
+    }
+}
